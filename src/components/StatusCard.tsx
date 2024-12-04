401d3048ed8f906f725ecc4fa90bbf6774a56a1f
Ben Gulapa <ben.gulapa@anglefinance.com.au>
2024-12-04 15:52:55 +0800

    add progress

--- src/components/StatusCard.tsx
+++ src/components/StatusCard.tsx
@@ -1,4 +1,11 @@
-import { Card, CardContent, CardHeader } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Stack,
+  Typography,
+} from "@mui/material";
 import CardTitleHeader from "./CardTitleHeader";
 
 interface IProps {
@@ -6,6 +13,8 @@ interface IProps {
 }
 
 const StatusCard = ({ loan }: IProps) => {
+  const status = ["Submitted", "Assessing", "Pending", "Decisioned", "Settled"];
+
   return (
     <Card variant="outlined" className="mb-2">
       <CardHeader
@@ -14,10 +23,35 @@ const StatusCard = ({ loan }: IProps) => {
         }
         sx={{ pb: 0 }}
       />
-      <CardContent
-        className="d-flex justify-content-between"
-        sx={{ pt: 1 }}
-      ></CardContent>
+      <CardContent>
+        <div className="d-flex w-100 mb-3">
+          {status.map((s, i) => (
+            <Box
+              key={i}
+              sx={{
+                background: i > 1 ? "inherit" : "green",
+                color: i > 1 ? "inherit" : "white",
+              }}
+              className="status-box d-flex align-items-center justify-content-center"
+            >
+              <Typography variant="caption">{s}</Typography>
+            </Box>
+          ))}
+        </div>
+
+        <Stack direction="row" alignItems="center">
+          <Box
+            className="status-done-progress"
+            sx={{ width: "30%", height: 4, background: "blue" }}
+          >
+            &nbsp;
+          </Box>
+          <Typography sx={{ width: "20%", textAlign: "center" }}>
+            {loan.creditStatus}
+          </Typography>{" "}
+          <Box className="status-future-progress" sx={{ width: "50%" }}></Box>
+        </Stack>
+      </CardContent>
     </Card>
   );
 };
