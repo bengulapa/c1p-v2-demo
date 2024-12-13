4e467d77fea18bb565b79fcd14a6822a0421d8c7
Ben Gulapa <ben.gulapa@anglefinance.com.au>
2024-12-13 12:10:53 +0800

    refactor theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,18 @@
 import { ThemeProvider } from "@emotion/react";
-import { createTheme, CssBaseline } from "@mui/material";
+import { CssBaseline } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 import ApplicationDetails from "./components/ApplicationDetails";
 import CreditDecision from "./components/CreditDecision";
 import CustomerDetails from "./components/CustomerDetails";
 import Overview from "./components/Overview";
 import Tasks from "./components/Tasks";
-import { Color } from "./styles/colors";
 import "./styles/styles.css";
+import { createOaTheme } from "./styles/theme";
 import "./styles/utility.css";
 import Dashboard from "./views/Dashboard";
 import Search from "./views/Search";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: Color.primary,
-    },
-    secondary: {
-      main: Color.secondary,
-    },
-    error: {
-      main: Color.red,
-    },
-    success: {
-      main: Color.green,
-    },
-    warning: {
-      main: Color.amber,
-    },
-  },
-  typography: {
-    fontFamily: "'Poppins', sans-serif",
-  },
-  components: {
-    MuiButton: {
-      defaultProps: {
-        size: "medium",
-      },
-    },
-    MuiFilledInput: {
-      defaultProps: {
-        margin: "dense",
-      },
-    },
-    MuiFormControl: {
-      defaultProps: {
-        size: "small",
-      },
-    },
-    MuiFormHelperText: {
-      defaultProps: {
-        margin: "dense",
-      },
-    },
-    MuiIconButton: {
-      defaultProps: {
-        size: "small",
-      },
-    },
-    MuiInputBase: {
-      defaultProps: {
-        margin: "dense",
-      },
-    },
-    MuiInputLabel: {
-      defaultProps: {
-        margin: "dense",
-        size: "small",
-      },
-    },
-    MuiListItem: {
-      defaultProps: {
-        dense: true,
-      },
-    },
-    MuiOutlinedInput: {
-      defaultProps: {
-        size: "small",
-      },
-    },
-    MuiFab: {
-      defaultProps: {
-        size: "small",
-      },
-    },
-    MuiTable: {
-      defaultProps: {
-        size: "small",
-      },
-    },
-    MuiTextField: {
-      defaultProps: {
-        margin: "dense",
-      },
-    },
-    MuiToolbar: {
-      defaultProps: {
-        variant: "dense",
-      },
-    },
-  },
-});
+const theme = createOaTheme();
 
 function App() {
   return (
