965b638a4db1510f9379141341592d8fac2570be
Ben Gulapa <bengulapa.dev@gmail.com>
2025-02-24 13:00:46 +0800

    update rating colors

--- src/components/ui/RangeDisplay.tsx
+++ src/components/ui/RangeDisplay.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Grid2 } from "@mui/material";
+import { Color } from "../../styles/colors";
 
 interface NumberSquaresProps {
   number: number;
@@ -8,13 +9,26 @@ interface NumberSquaresProps {
 const RangeDisplay: React.FC<NumberSquaresProps> = ({ number }) => {
   const maxRange = 10;
 
+  const getRatingColor = (rating: number) => {
+    if (rating <= 2) {
+      return Color.green;
+    } else if (rating <= 8) {
+      return Color.darkAmber;
+    } else if (rating <= 10) {
+      return Color.red;
+    } else {
+      return Color.lightGray;
+    }
+  };
+
   return (
     <Grid2 container spacing={0}>
       {Array.from({ length: maxRange }, (_, index) => index + 1).map((num) => (
         <Grid2 size={12 / maxRange} key={num} sx={{ p: 0, m: 0 }}>
           <Box
             sx={{
-              backgroundColor: num <= number ? "lightblue" : "lightgray",
+              backgroundColor:
+                num <= number ? getRatingColor(number) : "lightgray",
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
